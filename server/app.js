555fc3505f74a486f10248641c55ba0a7104afbe
Ztrebo14 <robertabon0414@gmail.com>
2024-05-31 19:02:15 +0800

    modified: added library

--- server/app.js
+++ server/app.js
@@ -4,9 +4,16 @@ const app = express()
 const port = 3000
 
 app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 
 app.get('/', (req, res) => {
     res.send('Got POST request on server')
+})
+
+app.get('/users', (req, res) => {
+    const userData = req.body
+    res.send(JSON.stringify(userData))
 })
 
 app.listen(port, () => {
